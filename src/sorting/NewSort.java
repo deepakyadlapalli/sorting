c017e158c93f60f30b16a462459856d42a44d3e3
tirdesh.pettugani <tirdesh.pettugani@CSELAB02-15.bennett.edu.in>
2017-08-22 11:34:58 +0530

    NewSort

---
+++ src/sorting/NewSort.java
@@ -0,0 +1,14 @@
+package sorting;
+import java.util.*;
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]={3,1,5,2,4,4};
+		Arrays.sort(a);
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
+		
+	}
+
+}
